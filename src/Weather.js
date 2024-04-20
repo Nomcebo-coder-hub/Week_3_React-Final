54c89d9acb9a9a3362eaae0e484da269d50e7058
Nomcebo Mthembu <nmthembu2797@gmail.com>
2024-04-20 20:51:40 +0000

    Removed text align center from css;  added weather link into index.js; removed axios link in Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React from "react";
-import axios from "axios";
 
 export default function Weather(props) {
   return (
@@ -12,5 +11,3 @@ export default function Weather(props) {
     </ul>
   );
 }
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=fbef01f4et1b02o0d25c27210a43ef3f&units=metric`;
-axios.get(url).then(showTemperature);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SearchEngine from "./SearchEngine";
+import Weather from "./Weather";
 
 import "./styles.css";
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <Weather />
     </div>
   );
 }
