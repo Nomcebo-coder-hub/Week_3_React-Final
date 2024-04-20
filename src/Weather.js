1c2b90730fbfbdbfb703fcdecfe53cd190ab9979
Nomcebo Mthembu <nmthembu2797@gmail.com>
2024-04-20 21:22:48 +0000

    Added usestate

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Weather(props) {
   return (
     <ul>
-      <li>Temperature:</li>
+      <li>Temperature: {temperature}</li>
       <li>Description:</li>
-      <li>Humidity:</li>
-      <li>Wind:</li>
+      <li>Humidity: {humidity}</li>
+      <li>Wind: {wind - speed}</li>
       <li></li>
     </ul>
   );
