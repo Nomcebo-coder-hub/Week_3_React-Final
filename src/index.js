72daaaca0504157f57ff42609e4d025324cde5f5
Nomcebo Mthembu <nmthembu2797@gmail.com>
2024-04-20 13:00:00 +0000

    Import styles.css to index.js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import styles from "./styles.css";
 import SearchEngine from "./SearchEngine";
+
+import "./styles.css";
 
 function App() {
   return <h1>Weather Search Engine</h1>;
