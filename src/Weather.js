aeeb19f62dfc4f2985596425279d74a1d42cdd9c
Nomcebo Mthembu <nmthembu2797@gmail.com>
2024-04-20 20:41:05 +0000

    Added api url and axios link to call out the url

--- src/Weather.js
+++ src/Weather.js
@@ -1,2 +1,5 @@
 import React from "react";
 import axios from "axios";
+
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=fbef01f4et1b02o0d25c27210a43ef3f&units=metric`;
+axios.get(url).then(showTemperature);
