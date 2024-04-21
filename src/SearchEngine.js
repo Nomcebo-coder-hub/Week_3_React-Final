b79511d54dcf0c5e61661d3256f45c73c50e889a
Nomcebo Mthembu <nmthembu2797@gmail.com>
2024-04-21 21:58:08 +0000

    Added apiUrl into SearchEngine.js

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -5,6 +5,7 @@ export default function SearchEngine() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    let url = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=fbef01f4et1b02o0d25c27210a43ef3f&units=metric`;
   }
 
   function updateCity(event) {
